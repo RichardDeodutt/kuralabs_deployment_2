c8e25df84f3dd129f4ddd318649a63e798a324d7
Richard Deodutt <richarddeodutt@gmail.com>
2022-09-26 05:12:15 -0400

    Test on another machine

--- Jenkinsfile
+++ Jenkinsfile
@@ -31,8 +31,6 @@ pipeline {
         steps {
           sh '''#!/bin/bash
             source testenv/bin/activate
-            export FLASK_APP=application
-            flask run &
             cypress run --spec test.spec.js
             '''
         }
--- test.spec.js
+++ test.spec.js
@@ -1,6 +1,6 @@
 describe('Heading', () => {
     it('has the right title', () => {
-        cy.visit('http://127.0.0.1:5000/')
+        cy.visit('http://url-shortener-dev.ap-northeast-1.elasticbeanstalk.com/')
 
         cy.get('title')
             .invoke('text')
